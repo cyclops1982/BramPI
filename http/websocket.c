604eae3ede46842124f45c1442bd5075cc2825bb
Ruben d'Arco <cyclops-github@prof-x.net>
2018-12-27 06:20:23 +0000

    Add h2o stuff, add HTML things for testing websocket

---
+++ http/websocket.c
@@ -0,0 +1,131 @@
+/*
+ * Copyright (c) 2014 DeNA Co., Ltd.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+#define  H2O_USE_LIBUV 0
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <h2o.h>
+#include <h2o/websocket.h>
+
+static void on_ws_message(h2o_websocket_conn_t *conn, const struct wslay_event_on_msg_recv_arg *arg)
+{
+
+    printf("Received message.\n");    
+    if (arg == NULL) {
+        h2o_websocket_close(conn);
+        return;
+    }
+
+    
+    if (!wslay_is_ctrl_frame(arg->opcode)) {
+        struct wslay_event_msg msgarg = {arg->opcode, arg->msg, arg->msg_length};
+        wslay_event_queue_msg(conn->ws_ctx, &msgarg);
+    }
+}
+
+static int on_req(h2o_handler_t *self, h2o_req_t *req)
+{
+    const char *client_key;
+
+    if (h2o_is_websocket_handshake(req, &client_key) != 0 || client_key == NULL) {
+        return -1;
+    }
+    h2o_upgrade_to_websocket(req, client_key, NULL, on_ws_message);
+    return 0;
+}
+
+static h2o_globalconf_t config;
+static h2o_context_t ctx;
+static h2o_accept_ctx_t accept_ctx;
+
+
+static void on_accept(h2o_socket_t *listener, const char *err)
+{
+    h2o_socket_t *sock;
+
+    if (err != NULL) {
+        return;
+    }
+
+    if ((sock = h2o_evloop_socket_accept(listener)) == NULL)
+        return;
+    h2o_accept(&accept_ctx, sock);
+}
+
+static int create_listener(void)
+{
+    struct sockaddr_in addr;
+    int fd, reuseaddr_flag = 1;
+    h2o_socket_t *sock;
+
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = htonl(0x7f000001);
+    addr.sin_port = htons(7890);
+
+    if ((fd = socket(AF_INET, SOCK_STREAM, 0)) == -1 ||
+        setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &reuseaddr_flag, sizeof(reuseaddr_flag)) != 0 ||
+        bind(fd, (struct sockaddr *)&addr, sizeof(addr)) != 0 || listen(fd, SOMAXCONN) != 0) {
+        return -1;
+    }
+
+    sock = h2o_evloop_socket_create(ctx.loop, fd, H2O_SOCKET_FLAG_DONT_READ);
+    h2o_socket_read_start(sock, on_accept);
+
+    return 0;
+}
+
+
+int main(int argc, char **argv)
+{
+    h2o_hostconf_t *hostconf;
+    h2o_pathconf_t *pathconf;
+
+    h2o_config_init(&config);
+    hostconf = h2o_config_register_host(&config, h2o_iovec_init(H2O_STRLIT("default")), 65535);
+    pathconf = h2o_config_register_path(hostconf, "/", 0);
+    h2o_create_handler(pathconf, sizeof(h2o_handler_t))->on_req = on_req;
+    h2o_context_init(&ctx, h2o_evloop_create(), &config);
+
+    /* disabled by default: uncomment the block below to use HTTPS instead of HTTP */
+    /*
+    if (setup_ssl("server.crt", "server.key") != 0)
+        goto Error;
+    */
+
+    accept_ctx.ctx = &ctx;
+    accept_ctx.hosts = config.hosts;
+
+    if (create_listener() != 0) {
+        fprintf(stderr, "failed to listen to 127.0.0.1:7890:%s\n", strerror(errno));
+        goto Error;
+    }
+
+    while (h2o_evloop_run(ctx.loop, INT32_MAX) == 0)
+        ;
+
+Error:
+    return 1;
+}
