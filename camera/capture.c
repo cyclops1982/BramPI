5be1193a0fb65f0a32663d1a140fc41f851a6920
Ruben <cyclops@debian9.lnd.prof-x.net>
2019-01-02 19:19:19 +0000

    Make image capture possible.
    The problem with teh canon 70D is that the 'default' storage medium is
    the Internal RAM. This means that images taken are not stored on the
    camera.
    
    The canon_enable_capture fixes this. Other canon's seem to want some
    other setting to do this.
    There's also example code (in libgphoto2) that uses half press and full
    press to take the photo, that *might* avoid the issue as well.

--- camera/capture.c
+++ camera/capture.c
@@ -19,6 +19,43 @@ static GPContext* create_context() {
 	
 }
 
+int canon_enable_capture (Camera *camera, GPContext *context) {
+	CameraWidget		*widget = NULL;
+	CameraWidgetType	type;
+	int			ret;
+
+	ret = gp_camera_get_single_config (camera, "capturetarget", &widget, context);
+	if (ret < GP_OK) {
+		fprintf (stderr, "camera_get_config failed: %d\n", ret);
+		return ret;
+	}
+
+	ret = gp_widget_get_type (widget, &type);
+	if (ret < GP_OK) {
+		fprintf (stderr, "widget get type failed: %d\n", ret);
+		goto out;
+	}
+
+	/* Now set the toggle to the wanted value */
+	char *stuff = "Memory card";
+
+	ret = gp_widget_set_value (widget, stuff);
+	if (ret < GP_OK) {
+		fprintf (stderr, "toggling Canon capture to %s failed with %d\n", stuff, ret);
+		goto out;
+	}
+	/* OK */
+	ret = gp_camera_set_single_config (camera, "capturetarget", widget, context);
+	if (ret < GP_OK) {
+		fprintf (stderr, "camera_set_config failed: %d\n", ret);
+		return ret;
+	}
+out:
+	gp_widget_free (widget);
+	return ret;
+}
+
+
 
 static int capture() {
 	int retval;
@@ -26,6 +63,10 @@ static int capture() {
 	GPContext *context = create_context();
 
 	CameraFilePath cameraFilePath;
+	CameraFile *file;
+	char *data;
+	unsigned long int size;
+
 	
 	gp_camera_new(&camera);
 	retval = gp_camera_init(camera, context);
@@ -34,6 +75,8 @@ static int capture() {
 		return retval;
 	}
 
+	canon_enable_capture(camera, context);
+
 	retval = gp_camera_capture(camera, GP_CAPTURE_IMAGE, &cameraFilePath, context);
 	if (retval != GP_OK) {
 		printf("Capture failed: %d\n", retval);
@@ -41,7 +84,32 @@ static int capture() {
 	}
 	printf("Return value: %d\n", retval);
 	printf("Path name: %s/%s\n", cameraFilePath.folder, cameraFilePath.name);
-	
+
+	retval = gp_file_new(&file);
+	if (retval != GP_OK) {
+		printf("Failed to gp_file_new: %d\n",retval);
+		return retval;		
+	}
+
+	retval = gp_camera_file_get(camera, cameraFilePath.folder, cameraFilePath.name, GP_FILE_TYPE_PREVIEW, file, context);
+	if (retval != GP_OK) {
+		printf("Failed to retrieve file: %d\n",retval);
+		return retval;
+	}
+
+	retval = gp_file_get_data_and_size(file, (const char**)&data, &size);
+	if (retval != GP_OK) {
+		printf("Failed to get data/size of file: %d\n",retval);
+		return retval;
+	}
+	printf("FIle size: %ld\n", size);
+
+	FILE *fd = fopen("test.cr2", "wb");
+	if (fd) {
+		int writen = fwrite(data, size, 1, fd);
+		printf("  fwrite size %ld, written %d\n", size, writen);
+		fclose(fd);
+	}
 
 	gp_camera_exit(camera, context);
 	return retval;
